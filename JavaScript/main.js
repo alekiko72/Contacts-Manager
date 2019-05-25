b88fe01a20095a01f996f571c2c820843cb06efa
erick1439 <44309686+erick1439@users.noreply.github.com>
2019-05-24 20:52:05 -0400

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+
+function completeInfo()
+{
+  var username = document.getElementById('userbox').value;
+  var password = document.getElementById('passwordbox').value;
+
+  if (username == null)
+    alert("Please insert username");
+
+  id (password == "")
+    alert("Please insert password");
+}
