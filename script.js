80789b2b37b7d2f3f2501d43b42e9cb56d761904
ThecopJo <46586935+ThecopJo@users.noreply.github.com>
2019-05-28 00:16:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -17,23 +17,21 @@ function addRow(){
     var cell2 = newRow.insertCell(2);
     var cell3 = newRow.insertCell(3);
     var cell4 = newRow.insertCell(4);
-    var cell5 = newRow.insertCell(5);
-    
+
     // Add the elements to each cell
-    cell0.innerHTML = (table.rows.length-1);
-    cell1.innerHTML = name;
-    cell2.innerHTML = number;
-    cell3.innerHTML = email;
+    cell0.innerHTML = name;
+    cell1.innerHTML = number;
+    cell2.innerHTML = email;
 
     // Create Buttons for edit and delete
     var edito = document.createElement("IMG");
     edito.setAttribute("src", "Images/leafedit.png");
-    cell4.appendChild(edito);
+    cell3.appendChild(edito);
 
     var deleto = document.createElement("IMG");
     deleto.setAttribute("src", "Images/leafdelete.png");
     deleto.setAttribute("class", "table-remove");
-    cell5.appendChild(deleto);
+    cell4.appendChild(deleto);
 
 }
 
