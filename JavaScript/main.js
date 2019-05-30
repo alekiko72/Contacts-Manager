371c8b9f1219562ab3760973b489fe08bec7eb0c
erick1439 <44309686+erick1439@users.noreply.github.com>
2019-05-29 23:24:12 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,9 +4,26 @@ function completeInfo()
   var username = document.getElementById('userbox').value;
   var password = document.getElementById('passwordbox').value;
 
-  if (username == null)
-    alert("Please insert username");
+  if (username == "" && password == "")
+  {
 
-  id (password == "")
-    alert("Please insert password");
+    alert("Please, enter your login information");
+    return false;
+  }
+
+  if (username == "")
+  {
+    alert("Please, insert your username");
+    return false;
+  }
+
+  if (password == "")
+  {
+    alert("Please, insert your password");
+    return false;
+  }
+
+  else
+    return true;
+
 }
