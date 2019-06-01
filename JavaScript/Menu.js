9f9679b3be6623cbcd58dab3a19bbff60a0eb42a
ThecopJo <46586935+ThecopJo@users.noreply.github.com>
2019-05-31 23:58:03 -0400

    Add files via upload

--- JavaScript/Menu.js
+++ JavaScript/Menu.js
@@ -26,8 +26,6 @@ function addRow(){
     cell2.innerHTML = number;
     cell3.innerHTML = email;
 
-    addContact(firstname, lastname, number, email);
-
     // Create Buttons for edit and delete
     var edito = document.createElement("IMG");
     edito.setAttribute("src", "Images/leafedit.png");
@@ -38,13 +36,40 @@ function addRow(){
     deleto.setAttribute("class", "table-remove");
     cell5.appendChild(deleto);
 
+    addContact(firstname, lastname, number, email);
+
+
 }
+
+/*function editRow(){
+
+    // Get all the elements the user inputs for the contacts table
+    var firstname = document.getElementById('FirstName').value;
+    var lastname = document.getElementById('LastName').value;
+    var number = document.getElementById('Number').value;
+    var email = document.getElementById('Email').value;
+
+    // Pass Table into Javascript
+    var table = document.getElementById("tabledo"),rIndex,cIndex;
+
+    for(var i = 0; i < table.rows.length; i++)
+    {
+        for(var j = 0; j < table.rows[i].cells.length; j++)
+        {
+            rIndex = this.parentElement.rowIndex;
+        }
+    }
+
+    table.rows[rIndex].cells[0].innerHTML = firstname;
+
+
+}*/
 
 function addContact(first, last, num, e)
 {
 	document.getElementById("contactAddResult").innerHTML = "";
 	
-	var jsonPayload = '{ firstName : "' + first + '", lastName : "' + last + '", phoneNumber : "' + num + '", email : "' + e + '", "userID" : ' + userID + '}';
+	var jsonPayload = '{ firstName : "' + first + '", lastName : "' + last + '", phoneNumber : "' + num + '", email : "' + e + '", "userId" : ' + userId + '}';
 	var url = urlBase + '/AddContact.' + extension;
 	
 	var xhr = new XMLHttpRequest();
@@ -114,9 +139,69 @@ function hideOrShow( elementId, showState )
 
 function doLogout()
 {
-	userID = 0;
+	userId = 0;
 
 	hideOrShow( "loggedInDiv", false);
 	hideOrShow( "accessUIDiv", false);
 	hideOrShow( "loginDiv", true);
 }
+
+const contactsTab = document.querySelector("#tabledo > tbody");
+
+function loadTable() {
+	const request = new XMLHttpRequest();
+
+	request.open("get", "contacts.json");
+	request.onload = () => {
+
+		try{
+			const json = JSON.parse(request.responseText);
+			populateTable(json);
+		}
+		catch(e) {
+			console.warn("Couldn't load table.");
+		}
+
+		request.send();
+
+	}
+
+}
+function populateTable(json){
+
+    // Gets rid of table that is initially there
+    while(contactsTab.firstChild){
+        contactsTab.removeChild(contactsTab.firstChild);
+    }
+
+    // Goes through every row and creates it
+    json.forEach((row) => {
+
+        // Create a new tr for each row
+        const tr = document.createElement("tr");
+
+        // create each cell 
+        row.forEach((cell) => {
+            const td = document.createElement("td");
+            //inserts the text into the cell
+            td.textContent = cell;
+            // appends each cell the the one before
+            tr.appendChild(td);
+        });
+
+
+        // Add the edit and delete buttons
+        var edita = document.createElement("IMG");
+        edito.setAttribute("src", "Images/leafedit.png");
+        cell4.appendChild(edito);
+
+        var deleta = document.createElement("IMG");
+        deleto.setAttribute("src", "Images/leafdelete.png");
+        deleto.setAttribute("class", "table-remove");
+        cell5.appendChild(deleto);
+
+        // Add the new row to the table
+        contactsTab.appendChild(tr);
+    });
+}
+document.addEventListener("DOMContentLoaded", () => { loadTable(); })
