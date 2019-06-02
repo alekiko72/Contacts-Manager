b01574693cd67688a81d05ffd4614ba36ac45c36
erick1439 <44309686+erick1439@users.noreply.github.com>
2019-06-02 01:56:02 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,9 @@
+var urlBase = 'http://www.cop4331.org/LAMPAPI';
+var extension = "php";
+
+var userId = 0;
+var firstName = "";
+var lastName = "";
 
 function completeInfo()
 {
@@ -23,7 +29,114 @@ function completeInfo()
     return false;
   }
 
-  else
-    return true;
+
+  doLogin();
+
+}
+
+function erase()
+{
+  document.getElementById('inputUserName').value = "";
+  document.getElementById('inputPassword').value = "";
+  document.getElementById('registrationAlert').innerHTML = "";
+
+
+}
+
+function register()
+{
+
+  var newUsername = document.getElementById('inputUserName').value;
+  var newPassword = document.getElementById('inputPassword').value;
+
+
+  if (newUsername == "" && newPassword == "")
+  {
+    document.getElementById('registrationAlert').innerHTML = "Fill the boxes";
+    return false;
+  }
+
+  if (newUsername == "")
+  {
+    document.getElementById('registrationAlert').innerHTML = "Enter Username";
+    return false;
+  }
+
+  if (newPassword == "")
+  {
+    document.getElementById('registrationAlert').innerHTML = "Enter Password";
+    return false;
+  }
+
+  var jsonPayload = '{"userId" : "' + userId + '", "username" : "' + newUsername + '", "password" : ' + newPassword + '}';
+  var url = urlBase + '/Registration.' + extension;
+
+  var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				console.log("you got boi");
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		 console.log(err.message);
+	}
+}
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+
+	var login = document.getElementById("userbox").value;
+	var password = document.getElementById("passwordbox").value;
+
+	document.getElementById("loginResult").innerHTML = "";
+
+  var jsonPayload = '{"username" : "' + login + '", "password" : "' + password + '"}';
+	var url = urlBase + '/Login.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+
+		var jsonObject = JSON.parse( xhr.responseText );
+
+		userId = jsonObject.id;
+
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+
+		window.name = firstName + " " + lastName;
+
+		document.getElementById("userbox").value = "";
+		document.getElementById("passwordbox").value = "";
+    window.location.href= "http://www.cop4331.org" + '/menu.html';
+
+
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+
+	}
 
 }
